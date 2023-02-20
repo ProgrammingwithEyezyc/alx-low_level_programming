bc6658879dd30afa9fc7dadb498641ebced36c28
ProgrammingwithEyezyc <eyezyccreed@yahoo.com>
2023-02-20 13:52:46 -0800

    Write a program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - code goes in here
+ * Description: get a random number
+ * Return: returning 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
